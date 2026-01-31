478604e1376a039a578815e6f075904ce17c6c64
Exedetor <exedetorheadgehog@gmail.com>
2026-01-31 20:21:01 -0300

    Agregado el funcionamiento de cam

---
+++ Assets/_MAIN/Scripts/ChoiceDetector.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ChoiceDetector : MonoBehaviour
+{
+    public RectTransform selector;
+    public RectTransform referenceObject1;
+    [SerializeField] private GameObject test;
+    
+
+    [SerializeField] private float timerChoiceTotal;
+    [SerializeField] private float timerCurrentTime;
+
+    private void Update()
+    {
+        Rect rectA = GetWorldRect(selector);
+        Rect rectB = GetWorldRect(referenceObject1);
+
+        if (rectA.Overlaps(rectB))
+        {
+            if (timerCurrentTime >= timerChoiceTotal)
+            {
+                test.SetActive(true);
+                timerCurrentTime = 0;
+            }
+            else
+                timerCurrentTime++;
+        }
+        else if (timerCurrentTime >= 0)
+            timerCurrentTime--;
+    }
+
+    private Rect GetWorldRect(RectTransform rt)
+    {
+        Vector3[] corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+        return new Rect(corners[0].x, corners[0].y,
+                        corners[2].x - corners[0].x,
+                        corners[2].y - corners[0].y);
+    }
+}
